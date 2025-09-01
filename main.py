5da35fca6243172343627f0b303ca2b84eaababc
pankudlaty <pkownacki90@gmail.com>
2025-09-01 10:22:03 +0200

    Adding Player class, drawing player(white triangle)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 import pygame
 from constants import *
 
@@ -9,12 +10,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
