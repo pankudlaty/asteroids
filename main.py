ce153389f1e7f8c294cd66cbd376be0f14dea296
pankudlaty <pkownacki90@gmail.com>
2025-09-08 16:23:42 +0200

    Pygame groups

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     clock = pygame.time.Clock()
     dt = 0
+    Player.containers = (updateable,drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updateable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
